240a37394f03c09ef3375de0b3d7160649b95279
dk <deinkoks@cert.ccc.de>
2021-12-28 22:32:48 +0100

    image tuning

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let angle = 0;
 const speed = 100;
 
 function advance() {
-    angle = (angle + 1) % 360;
+    angle = (angle + 0.1) % 360;
     document.getElementById("bg").style.filter = `hue-rotate(${angle}deg)`;
     setTimeout(advance, speed)
 };
